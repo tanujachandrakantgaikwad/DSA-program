e93c2afa1a23b5aef2abdcf3eb8cf68889f2a61c
Tanuja Chandrakant Gaikwad <tanujagaikwad5585@gmail.com>
2024-07-08 20:27:26 +0530

    Add files via upload

---
+++ to search pro porfomace.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+  int a[100],n,i,num;
+  printf("enter limit");
+  scanf("%d",&n);
+  printf("enter n number");
+  for(i=0;i<n;i++)
+   {
+   	 scanf("%d",&a[i]);
+   }
+   printf("enter number to search");
+   scanf("%d",&num);
+   for(i=0;i<n;i++)
+    {
+    	if(a[i]==num)
+    	{
+    		 break;
+		}
+	}
+	if(i==n)
+	 printf("nemuber not found ");
+	else
+	  printf("number found");
+	 if(i==0)
+	  printf("\n best case");
+	else if(i==n-1)
+	  printf("\n worst case");
+	else
+	 printf("\n average case");
+}
